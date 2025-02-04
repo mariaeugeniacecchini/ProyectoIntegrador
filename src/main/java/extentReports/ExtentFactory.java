f34b6251ff4405354fd7afbc24438a33b0751290
Gian-vega <gian.vega@ayi.group>
2025-02-04 15:21:54 -0300

    Initial commit

---
+++ src/main/java/extentReports/ExtentFactory.java
@@ -0,0 +1,14 @@
+package extentReports;
+
+import com.aventstack.extentreports.ExtentReports;
+
+public class ExtentFactory {
+    public static ExtentReports getInstance(){
+        ExtentReports extent = new ExtentReports();
+        extent.setSystemInfo("Selenium Version", "4.28.1");
+        extent.setSystemInfo("SO", "Windows 11");
+        extent.setSystemInfo("Navegador", "Chrome");
+        extent.setSystemInfo("ENV", "QA");
+        return extent;
+    }
+}
